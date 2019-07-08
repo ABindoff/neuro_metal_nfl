56c25fbb3a3acdeb025fd2b9faa1566e6c7ad124
Aidan Bindoff <aidan.bindoff@utas.edu.au>
2019-07-08 19:08:57 +1000

    marginalise over region

--- junk.R
+++ junk.R
@@ -7,17 +7,18 @@ d <- expand.grid(metal = c(-0.5,0,.5,1),
                  id = c("a", "b", "c", "d", "e", "f", "g","h","i","j","k", "aa", "bb", "cc", "dd", "ee", "ff", "gg","hh","ii","jj","kk"))
 d$group <- c(rep(0, 11*20), rep(1, 11*20))
 
-d$conc <- 0.3*d$metal -.9*d$region + 1.2*d$metal*d$region - 1.2*d$metal*d$group -1*d$region*d$group + rnorm(nrow(d), 3, 1)
+d$conc <- 0.3*d$metal -.9*d$region + -0.2*d$group + 1.2*d$metal*d$region - 2.2*d$metal*d$group + rnorm(nrow(d), 3, 1)
 
 d <- d %>% mutate(ranef = rep(rnorm(22, 0, 2), each = 20),
                   conc = conc + ranef)
 
 
 ggplot(d, aes(x = factor(metal), y = conc, colour = factor(region))) +
-  geom_boxplot()
+  geom_boxplot() +
+  facet_wrap(~group)
 
 
-m1.lin <- lmer(conc ~ metal*region*group + (1|id), d)
+m1.lin <- lmer(conc ~ metal*region+ metal*group + (1|id), d)
 summary(m1.lin)
 
 d <- d %>%
@@ -125,3 +126,70 @@ p3 <- ggplot(k, aes(x = y1, y = y2, colour = factor(group), size = att, alpha = 
 #plotly::plotly_json(p1)
 
 plotly::ggplotly(p3)
+
+
+A <- select(d, group,  metal, region, conc, id) %>% 
+  tidyr::spread(metal, conc)  %>%
+  group_by(region) %>%
+  mutate(Al = scale(Al),
+         Zn = scale(Zn),
+         Fe = scale(Fe),
+         Rb = scale(Rb)) %>%
+  ungroup()
+
+set.seed(888)
+m1 <- Rtsne(A[,-c(1,2)], dim = 2,
+            perplexity = 7,
+            max_iter = 10000, 
+            theta = 0.05)
+
+k <- lapply(metals, foo)
+k <- dplyr::bind_rows(k)
+k$Metal <- factor(k$Metal, levels = metals)
+
+p4 <- ggplot(k, aes(x = y1, y = y2, colour = region, shape = factor(group), size = att, alpha = att/2, frame = Metal, scale = 1.5)) +
+  geom_point() +
+  geom_point(size = .5, alpha = .8, show.legend = FALSE) +
+  theme_minimal()
+
+#plotly::plotly_json(p1)
+
+plotly::ggplotly(p4)
+
+p4.1 <- ggplot(k, aes(x = y1, y = y2, colour = id, shape = factor(group), size = att, alpha = att/2, frame = Metal, scale = 1.5)) +
+  geom_point() +
+  geom_point(size = .5, alpha = .8, show.legend = FALSE) +
+  theme_minimal()
+
+#plotly::plotly_json(p1)
+
+plotly::ggplotly(p4.1)
+
+A <- select(d, group,  metal, region, fconc, id) %>% 
+  tidyr::spread(metal, fconc)  %>%
+  group_by(region) %>%
+  mutate(Al = scale(Al),
+         Zn = scale(Zn),
+         Fe = scale(Fe),
+         Rb = scale(Rb)) %>%
+  ungroup()
+
+set.seed(888)
+m1 <- Rtsne(A[,-c(1,2)], dim = 2,
+            perplexity = 7,
+            max_iter = 10000, 
+            theta = 0.05)
+
+k <- lapply(metals, foo)
+k <- dplyr::bind_rows(k)
+k$Metal <- factor(k$Metal, levels = metals)
+
+p5 <- ggplot(k, aes(x = y1, y = y2, colour = region, shape = factor(group), size = att, alpha = att/2, frame = Metal, scale = 1.5)) +
+  geom_point() +
+  geom_point(size = .5, alpha = .8, show.legend = FALSE) +
+  theme_minimal()
+
+#plotly::plotly_json(p1)
+
+plotly::ggplotly(p5)
+
